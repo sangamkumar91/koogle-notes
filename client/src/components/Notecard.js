2c550dde26429fbc950ceb49f2b254f322fec877
Sangam Kumar <sangamkumar91@Sangams-MacBook-Pro.local>
2018-08-29 20:56:49 -0400

    UI done

--- client/src/components/Notecard.js
+++ client/src/components/Notecard.js
@@ -1,13 +1,27 @@
 import React, { Component} from "react";
 import "./../css/Notecard.css";
+import Task from "./Task.js";
 
 class Notecard extends Component{
   render(){
     return(
       <div className="notecard-container">
         <div className="notecard">
-          <input type="text" className="note-title"/>
-          <div className="task-list"></div>
+          <div className="note-checkbox">
+            <i className="material-icons font-large checked-note">check_circle</i>
+            <i className="material-icons font-large unchecked-note">check_circle_outline</i>
+          </div>
+          <div contenteditable="true" className="note-title" placeholder="Title"/>
+          <div className="task-list">
+            <Task/>
+            <Task/>
+            <Task/>
+            <Task/>
+            <button className="add-task-button">
+              <i className="material-icons font-large">add_box</i>
+              <span className="font-large"></span>
+            </button>
+          </div>
         </div>
       </div>
     );
--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,10 +1,14 @@
 import React, { Component} from "react";
+import "./../css/Task.css";
 
 class Task extends Component{
   render(){
     return(
       <div className="task">
-        Task
+        <button className="task-complete">
+          <i className="material-icons font-large">check_box_outline_blank</i>
+        </button>
+        <div contenteditable="true" type="text" className="task-input" placeholder="..."/>
       </div>
     );
   }
